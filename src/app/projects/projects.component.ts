a1cfe661656d431719c51b40e87f7c5a73bd4789
Felipe Nogueira <felipe.leite23@gmail.com>
2025-04-28 20:19:05 -0300

    feat: update project display and styles
    - Modified package.json to allow serving on all hosts.
    - Added new SVG icons for Angular, C++, GitHub, PlatformIO, React, and TypeScript.
    - Updated projects.component.html to dynamically display project cards with images, titles, descriptions, and technology icons.
    - Enhanced projects.component.scss with new styles for project cards and grid layout.
    - Implemented projects.component.ts to include project data with relevant details and technology links.
    - Changed global styles in styles.scss for card background colors in light and dark themes.
    - Added new background images for projects.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,11 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss'],
+  imports: [
+    CommonModule,
+  ],
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      image: 'assets/projects/backgrounds/shokanri.png',
+      title: 'ShoKanri',
+      description: 'ShoKanri é um sistema de gerenciamento de finanças pessoais que visa ajudar os usuários a controlar suas despesas e receitas de forma simples e eficiente. O sistema permite o registro de transações financeiras, categorização de despesas, geração de relatórios e gráficos para análise financeira.',
+      link: 'sho-kanri-front.vercel.app',
+      technologies: [
+        { name: 'GitHub', icon: 'assets/icons/github.svg', link: 'https://github.com/FelpLiet/ShoKanri-Front' },
+        { name: 'Angular', icon: 'assets/icons/angular.svg' },
+        { name: 'TypeScript', icon: 'assets/icons/typescript.svg' },
+      ]
+    },
+    {
+      image: 'assets/projects/backgrounds/sescomp.png',
+      title: 'Site SESCOMP 2024',
+      description: 'Este é o site oficial da Semana de Engenharia de Software e Ciência da Computação (SESCOMP) de 2024, um evento que conecta alunos, professores e profissionais apaixonados por tecnologia. O site foi desenvolvido para fornecer informações sobre o evento, programação, atividades e facilitar a inscrição dos participantes.',
+      technologies: [
+        { name: 'GitHub', icon: 'assets/icons/github.svg', link: 'https://github.com/FelpLiet/sescomp' },
+        { name: 'React', icon: 'assets/icons/react.svg' },
+        { name: 'TypeScript', icon: 'assets/icons/typescript.svg' },
+      ]
+    },
+    {
+      image: 'assets/projects/backgrounds/garu.jpg',
+      title: 'Projeto GARU – Gestão de Acesso as Residências Universitárias',
+      description: 'Desenvolvimento de PCBs e firmware do ESP32 para gerenciamento de recursos em ambientes acadêmicos. O projeto GARU visa otimizar o controle de acesso e a gestão de recursos em residências universitárias, proporcionando uma solução eficiente e segura para os usuários.',
+      technologies: [
+        { name: 'GitHub', icon: 'assets/icons/github.svg' },
+        { name: 'PlatformIO', icon: 'assets/icons/platformio.svg' },
+        { name: 'C++', icon: 'assets/icons/c++.svg' },
+      ]
+    },
+  ];
 }
