505e7912e4fbdc381f79a9d3cdc83f9565438ac2
Felipe Nogueira <felipe.leite23@gmail.com>
2025-04-29 02:11:17 -0300

    feat: add emailjs integration for contact form and update about/contact sections with new content and styles

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    emailJs: {
+      userId:   'BTC5popIGgV_Xl0ah',
+      serviceId:'service_gbskyng',
+      privateKey: 'SEU_PRIVATE_KEY_EMAILJS_PROD',
+      templateId:'template_lcdhlon'
+    }
+  };
